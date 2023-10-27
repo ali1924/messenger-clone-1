d86d6046bc69a65b5f44578e9aaf9f97130f05c5
ali1924 <mohammadali229987@gmail.com>
2023-10-27 11:50:17 +0600

    Logo and heading done

---
+++ app/(site)/page.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+const page = () => {
+    return (
+        <div className="
+        min-h-full
+        flex
+        flex-col
+        justify-center
+        py-12
+        sm:px-6
+        lg:px-8
+        md:px-8
+        bg-gray-100
+
+        ">
+            <div className="sm:mx-auto sm:w-full lg:max-w-md">
+                {/* Logo */}
+                <Image
+                    src="/images/logo.png"
+                    alt="logo"
+                    width="48"
+                    height="48"
+                    className="mx-auto w-auto"
+                />
+                {/* Heading */}
+                <h2 className="
+                  text-3xl
+                  font-bold
+                  text-center
+                  tracking-tight
+                  text-gray-900
+                ">
+                    Sign in your account
+                </h2>
+            </div>
+        </div>
+    );
+};
+
+export default page;
